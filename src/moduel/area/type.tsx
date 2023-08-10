de736473c01bad1db7f2be5a501b7471fce225c9
haha861924 <40877568+haha861924@users.noreply.github.com>
2023-08-10 23:46:32 +0800

    feat: area page (#4)
    * refactor: 💡 navbar
    
    * feat: 🎸 area page
    
    * fix: 🐛 router
    
    * fix: 🐛 useless
    
    * fix: 🐛 tailwind css can't dynamic
    
    * style: 💄 fix navbar z-index
    
    * feat: 🎸 area type add boss
    
    * fix: 🐛 build error

---
+++ src/moduel/area/type.tsx
@@ -0,0 +1,7 @@
+export interface AreaItemsType {
+  name: string;
+  slug: string;
+  photo: string;
+  boss?: string;
+  bossPhoto?: string;
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,21 +1,24 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App';
+import Area from '@/page/Area';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/map',
-    element: <div>Map</div>,
-  },
-  {
-    path: '/food',
-    element: <div>Food</div>,
-  },
-  {
-    path: '/monster',
-    element: <div>Monster</div>,
+    children: [
+      {
+        path: '/area',
+        element: <Area />,
+      },
+      {
+        path: '/food',
+        element: <div>Food</div>,
+      },
+      {
+        path: '/monster',
+        element: <div>Monster</div>,
+      },
+    ],
   },
 ]);
