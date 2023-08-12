1281ee8b181aec954ee37285ca04d3a5cbb80dbe
Elvina <qq861924@gmail.com>
2023-08-13 02:04:27 +0800

    feat: 🎸 data to json

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,10 +1,15 @@
-import { CATEGORIES } from '@/module/inventory/constants';
+import Loading from '@/components/Loading';
+import useGetAPI from '@/hooks/useGetAPI';
 import { CategoryType } from '@/module/inventory/types';
 
 export const Category = () => {
+  const { isLoading, data: categories } = useGetAPI('/data/categories.json');
+
+  if (isLoading) return <Loading />;
+
   return (
     <div className="flex flex-wrap items-center justify-start p-24">
-      {CATEGORIES.map((item: CategoryType, index: number) => (
+      {categories.map((item: CategoryType, index: number) => (
         <div key={index} className="m-2 rounded-lg bg-white p-2 text-teal-500">
           {item.name}
         </div>
--- src/page/Area.tsx
+++ src/page/Area.tsx
@@ -1,12 +1,19 @@
 import { Card, CardList } from '@/components/Card';
-import { areaItems } from '@/module/area';
+import Loading from '@/components/Loading';
+import useGetAPI from '@/hooks/useGetAPI';
 import { AreaItemsType } from '@/module/area/type';
 
+type Props = AreaItemsType;
+
 export const Area = () => {
+  const { isLoading, data: areaItems } = useGetAPI('/data/areas.json');
+
+  if (isLoading) return <Loading />;
+
   return (
     <CardList>
-      {areaItems.map(({ name, ...restArea }: AreaItemsType) => (
-        <Card key={name} title={name} {...restArea} />
+      {areaItems.map(({ name, photo, ...restArea }: Props) => (
+        <Card key={name} title={name} {...restArea} photo={`area/${photo}.webp`} />
       ))}
     </CardList>
   );
