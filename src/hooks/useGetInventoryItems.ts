a315048df0d7c9c2dfef574116239afbf3d35093
haha861924 <40877568+haha861924@users.noreply.github.com>
2023-08-14 16:04:48 +0800

    Feature/all categories (#16)
    * fix: 🐛 category name
    
    * feat: 🎸 reset body style
    
    * feat: 🎸 all categories

--- src/hooks/useGetInventoryItems.ts
+++ src/hooks/useGetInventoryItems.ts
@@ -5,6 +5,8 @@ import { Category } from '@/module/inventory';
 export const useGetInventoryItems = (category: Category | null) => {
   const { data = [], ...rest } = useQuery([getInventoryItems.name], () => getInventoryItems());
 
+  if (category === 'all') return { data, ...rest };
+
   const filtered = category ? data.filter((item) => item.category === category) : data;
 
   return { data: filtered, ...rest };
