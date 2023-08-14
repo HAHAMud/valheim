4394b4f67fd6e290be56e2753836e93cb2eacee0
Elvina <qq861924@gmail.com>
2023-08-14 14:45:21 +0800

    refactor: 💡 card component style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,58 @@
+import {
+  Card as CardMui,
+  CardCover,
+  CardContent,
+  Typography,
+  CardOverflow,
+  Divider,
+} from '@mui/joy';
+import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
 import { CardType } from './types';
-import { CardBg } from '@/assets/svg';
 
 type Props = CardType;
 
-export function Card({ photo, slug, title, extra }: Props) {
+export function Card({ photo, slug, title, extra, info, minHeight = 160, width = 200 }: Props) {
   return (
-    <div className="relative m-6 max-w-xs flex-shrink-0 overflow-hidden rounded-lg bg-teal-500 shadow-lg">
-      <div className="absolute left-0 top-0 mb-8 h-full w-full scale-x-150 opacity-10">
-        <CardBg />
-      </div>
-
-      <div className="relative flex items-center justify-center px-10 pt-10">
-        <img className="relative w-full" src={`${photo}`} alt="" />
-      </div>
-
-      <div className="relative mt-6 px-6 pb-6 text-white">
-        <span className="-mb-1 block opacity-75">{slug}</span>
-        <div className="flex justify-between">
-          <span className="block text-xl font-semibold">{title}</span>
-          {extra && (
-            <span className="flex items-center rounded-full bg-white px-3 py-2 text-xs font-bold leading-none text-teal-500">
-              {extra}
-            </span>
-          )}
-        </div>
-      </div>
-    </div>
+    <CardMui sx={{ minHeight, width }}>
+      <CardCover>
+        <img
+          src={photo}
+          srcSet={photo}
+          loading="lazy"
+          alt={title}
+          style={{
+            width: '100%',
+            height: '100%',
+            objectPosition: 'center',
+          }}
+        />
+      </CardCover>
+      <CardCover
+        sx={{
+          background:
+            'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
+        }}
+      />
+      <CardContent sx={{ justifyContent: 'flex-end' }}>
+        <Typography level="title-lg" textColor="#fff" mb={1}>
+          {title}
+        </Typography>
+        <Typography startDecorator={<LocationOnRoundedIcon />} textColor="neutral.300">
+          {slug}
+        </Typography>
+      </CardContent>
+      <CardOverflow variant="soft" sx={{ bgcolor: 'background.level1' }}>
+        <Divider inset="context" />
+        <CardContent orientation="horizontal">
+          <Typography level="body-xs" fontWeight="md" textColor="text.secondary">
+            {info}
+          </Typography>
+          <Divider orientation="vertical" />
+          <Typography level="body-xs" fontWeight="md" textColor="text.secondary">
+            {extra}
+          </Typography>
+        </CardContent>
+      </CardOverflow>
+    </CardMui>
   );
 }
--- src/components/Card/CardList.tsx
+++ src/components/Card/CardList.tsx
@@ -1,7 +1,15 @@
 import { PropsWithChildren } from 'react';
+import { Box } from '@mui/joy';
 
 type Props = PropsWithChildren<{}>;
 
 export function CardList({ children }: Props) {
-  return <div className="flex flex-wrap items-center justify-start p-24">{children}</div>;
+  return (
+    <Box
+      component="ul"
+      sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', p: 0, m: 6, justifyContent: 'flex-start' }}
+    >
+      {children}
+    </Box>
+  );
 }
